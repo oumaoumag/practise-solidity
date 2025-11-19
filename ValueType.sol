d1d71de30a25f3fe3b2923f0b4a9f8784bd90c79
oumaoumag <ouma.godwin10@gmail.com>
2025-11-19 07:40:06 +0300

    Add value types and data references

---
+++ ValueType.sol
@@ -0,0 +1,25 @@
+// Variables can be divided into two Categories
+// 1. Value Type - Variables that store actual data such as int, bool, and addresses.
+// 2. Reference Types - Variables that store a reference to the data's location, like mapping.
+
+// SPDX-License-Identifier: mit
+pragma solididy ^0.8,0;
+
+contract Example {
+    mapping(int256 => address) ap;
+
+    function types() public {
+        uint256 a = 1;
+        uint256 b = a;
+        a = 2; // a is updated, but stays the same
+        b = 4; // b is updated, but a stays the same
+
+        map[1] = address(0x123)
+    }
+}
+
+// What happens when you assign a reference type value to another variable?
+// Reference type are more complex data types that hold a refrence to the location of the data, rather
+// than the data itself. They  incluse data structures like arrays, structs, and mapping. 
+// When you assign reference type to another variable, both variables refer to the same data, and changes to
+// one variable will affect the other.
