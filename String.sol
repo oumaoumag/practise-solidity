a47b81baacb833233206c847807d260d126bc7eb
oumaoumag <ouma.godwin10@gmail.com>
2025-11-19 07:48:21 +0300

    Add strings explantions

---
+++ String.sol
@@ -0,0 +1,20 @@
+// Strings
+// A 'String' is use to represent textual data, like the name "GUA", date "Dec 18, 2000", or any tex.
+
+// To declare a string we use the keyword 'string' then express the textual content using either single or double quotes.
+// We use '=' to assign the new value to it.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract LearningStrings {
+    string car = "BMW";
+    string text;
+
+    // assign a value to a string variable using the function
+    function setText () public returns (string memory) {
+        text = "Hello World";
+        return text;
+    }
+
+}
